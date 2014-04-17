911b33a7acc0c863981b4c41ee06fd686225577e
Sally€酮خد㐁ᠡꀈ༑㘚郂 <278347137@qq.com>
2014-04-17 13:26:35 +0800

    upload class4issue通€㐀ᠠꀇ༐بة㊣郂

---
+++ RADPRO_€酮خد㐁ᠡꀈ༑㘚郂/src/gbclass_€酮خد㐁ᠡꀈ㘚郂4contenttest.java
@@ -0,0 +1,16 @@
+
+public class gbclass_€酮خد㐁ᠡꀈ㘚郂
+{
+	public static void main(String[] args) 
+	 {
+	  int i,j,k;
+	  System.out.println("i  j  k");
+	  for (i=0; i<2; i++)
+	   for (j=0; j<2; j++)
+	    for (k=0; k<2; k++)
+	    {
+	     System.out.print(i+"€酮خد㐁ᠡꀈ༑㘚郂"+j+"  "+k);
+	     System.out.println();
+	    }
+	 }
+}
