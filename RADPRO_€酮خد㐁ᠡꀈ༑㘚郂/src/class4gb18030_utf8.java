c7419a903038206f51d3c854724a1c523807f05e
Sally€酮خد㐁ᠡꀈ༑㘚郂 <278347137@qq.com>
2014-04-17 13:54:07 +0800

    upload class4gb18030_utf8

---
+++ RADPRO_€酮خد㐁ᠡꀈ༑㘚郂/src/class4gb18030_utf8.java
@@ -0,0 +1,15 @@
+
+public class class4gb18030_utf8 {
+	public static void main(String[] args) 
+	 {
+	  int i,j,k;
+	  System.out.println("i  j  k");
+	  for (i=0; i<2; i++)
+	   for (j=0; j<2; j++)
+	    for (k=0; k<2; k++)
+	    {
+	     System.out.print(i+"€酮خد㐁ᠡꀈ༑㘚郂"+j+"  "+k);
+	     System.out.println();
+	    }
+	 }
+}
