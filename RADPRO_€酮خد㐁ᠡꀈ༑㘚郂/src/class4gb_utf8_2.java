24763b447e027e2e786cf1e4a4d37b1a340e51d0
Sally€酮خد㐁ᠡꀈ༑㘚郂 <278347137@qq.com>
2014-04-17 13:58:34 +0800

    upload class4gb_uft8_2

---
+++ RADPRO_€酮خد㐁ᠡꀈ༑㘚郂/src/class4gb_utf8_2.java
@@ -0,0 +1,15 @@
+
+public class class4gb_utf8_2 {
+	public static void main(String[] args) 
+	 {
+	  int i,j,k;
+	  System.out.println("i  j  k");
+	  for (i=0; i<2; i++)
+	   for (j=0; j<2; j++)
+	    for (k=0; k<2; k++)
+	    {
+	     System.out.print(i+"€酮خد㐁ᠡꀈ༑㘚郂"+j+"  "+k);
+	     System.out.println();
+	    }
+	 }
+}
