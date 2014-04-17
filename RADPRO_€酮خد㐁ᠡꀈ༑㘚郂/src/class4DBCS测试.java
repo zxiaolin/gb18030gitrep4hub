3410dd820d06161831ce3d9feae759dd6b624dd5
Sally€酮خد㐁ᠡꀈ༑㘚郂 <278347137@qq.com>
2014-04-17 13:39:04 +0800

    upload class4DBCS测试

---
+++ RADPRO_€酮خد㐁ᠡꀈ༑㘚郂/src/class4DBCS测试.java
@@ -0,0 +1,15 @@
+
+public class class4DBCS≤‚ ‘ {
+	public static void main(String[] args) 
+	 {
+	  int i,j,k;
+	  System.out.println("i  j  k");
+	  for (i=0; i<2; i++)
+	   for (j=0; j<2; j++)
+	    for (k=0; k<2; k++)
+	    {
+	     System.out.print(i+"  "+j+"≤‚ ‘ "+k);
+	     System.out.println();
+	    }
+	 }
+}
